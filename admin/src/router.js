1e1fd3423a82332a1c85d928cc45962f12cf450b
tokukanya116 <tokukanya116@gmail.com>
2020-11-07 18:12:11 +0800

    修改路由为通用路由

--- admin/src/router.js
+++ admin/src/router.js
@@ -7,7 +7,7 @@ import CategoryList from './views/CategoryList.vue'
 Vue.use(Router)
 
 export default new Router({
-    // mode:'history',
+    mode:'history',
     routes: [
         {
             path: '/',
--- admin/src/views/CategoryEdit.vue
+++ admin/src/views/CategoryEdit.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
-    <h1>新建分类 {{ id }}</h1>
+    <h1>新建分类</h1>
     <el-form label-width="120px" @submit.native.prevent="save">
+      <el-form-item label="上级分类">
+        <!-- 父子分类关联应该是唯一指定id关联 -->
+        <el-select v-model="model.parent">
+          <el-option v-for="item in parents" :key="item._id" 
+          :label="item.name" :value="item._id"></el-option>
+        </el-select>
+      </el-form-item>
       <el-form-item label="名称">
         <el-input v-model="model.name"></el-input>
       </el-form-item>
@@ -26,6 +33,7 @@ export default {
   data() {
     return {
       model: {},
+      parents: [], //父级分类的选项
     };
   },
   methods: {
@@ -33,12 +41,12 @@ export default {
       let res;
       if (this.id) {
         // 如果id存在,替换值
-        res = await this.$http.put(`categories/${this.id}`, this.model);
+        res = await this.$http.put(`rest/categories/${this.id}`, this.model);
       } else {
         // 如果不存在,新建
-        res = await this.$http.post("categories", this.model);
+        res = await this.$http.post("rest/categories", this.model);
       }
-      this.$router.push("/categories/list");
+      this.$router.push("rest/categories/list");
       this.$message({
         type: "successs",
         message: "保存成功",
@@ -47,11 +55,17 @@ export default {
     },
     //获取选择编辑的id名称
     async fetch() {
-      const res = await this.$HTTP.get(`categories/${this.id}`);
+      const res = await this.$http.get(`rest/categories/${this.id}`);
       this.model = res.data;
+    },
+    //获取父级选项
+    async fetchParents() { 
+      const res = await this.$http.get(`rest/categories`);
+      this.parents = res.data;
     },
   },
   created() {
+    this.fetchParents();
     this.id && this.fetch();
   },
   // components:{},
